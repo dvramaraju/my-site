2e0c4105ed1c4e2d724aa3586fa081663132de4b
dvramaraju <ramarajudv21@gmail.com>
2020-11-26 16:46:40 +0530

    style

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 
 function tick() {
   const element = (
-    <div>
-      <h2>Date: {new Date().toLocaleTimeString()}, Date: {new Date().toLocaleDateString()}</h2>
-      <App />,
+    <div className='dark-mode'>
+      <h2 style={{ backgroundColor: 'green', fontSize: 30, textAlign: "center" }}>Date: {new Date().toLocaleTimeString()}, Date: {new Date().toLocaleDateString()}</h2>
+      <App />
     </div>
   );
   ReactDOM.render(element, document.getElementById('root'));
